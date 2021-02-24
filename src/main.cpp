0f3ec0279ee9bbd4ee2c62dcc8e470a916abfa83
SamyHasRoot <samuel.r.lindell@gmail.com>
2021-02-24 18:34:20 +0100

    Fix dll loading

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,31 @@
 #include <memory>
 #include <string>
 #include <sstream>
-#include <dlfcn.h>
 #include "replies.h"
 #include <filesystem>
 namespace fs = std::filesystem;
 
+#ifdef _WINDOWS
+#include <windows.h>
+#include <stdio.h>
+static const char* kDL_PREFIX = "";
+static const char* kDL_SUFFIX = ".dll";
+#else
+#include <dlfcn.h>
+static const char* kDL_PREFIX = "LIB";
+static const char* kDL_SUFFIX = ".so";
+#endif
 
 std::shared_ptr<BaseObject> Load(std::string file_path, ReplyHandler& rp) {
 	std::cout << "Loading: " << file_path << "\n";
-
 #ifdef _WINDOWS
-#include <windows.h>
 	typedef void (__stdcall *destroy_ptr)(BaseObject *);
-	HINSTANCE handle = LoadLibrary(file_path);
+	HINSTANCE handle = LoadLibrary(file_path.c_str());
 
 	BaseObject* (__stdcall *create)(ReplyHandler&);
 
-	create = (BaseObject* (__stdcall *)(ReplyHandler&))GetProcAddress(hGetProcIDDLL, "create_object");
-	destroy_ptr destroy = (void (__stdcall *)(BaseObject&))GetProcAddress(hGetProcIDDLL, "destroy_object");
+	create = (BaseObject* (__stdcall *)(ReplyHandler&))GetProcAddress(handle, "create_object");
+	destroy_ptr destroy = (void (__stdcall *)(BaseObject*))GetProcAddress(handle, "destroy_object");
 #else
 	typedef void (*destroy_ptr)(BaseObject *);
 	void* handle = dlopen(file_path.c_str(), RTLD_LAZY);
@@ -41,7 +48,7 @@ void LoadToMap(const std::string& base_dir, std::map<std::string, std::shared_pt
 	auto names = {"button", "container", "door", "light", "start_button"};
 	for (auto name : names) {
 		std::ostringstream file_path;
-		file_path << base_dir << "objects/" << "lib" << name << ".so";
+		file_path << base_dir << "objects/" << kDL_PREFIX << name << kDL_SUFFIX;
 		type_to_obj[name] = Load(file_path.str(), rp);
 	}
 }
@@ -55,7 +62,7 @@ int main(int, char *argv[]) {
 	std::map<std::string, std::shared_ptr<BaseObject>> type_to_obj;
 	type_to_obj["obj"] = std::make_shared<BaseObject>(state->reply_handler);
 
-	LoadToMap(base_dir, type_to_obj, state->reply_handler);
+	LoadToMap(base_dir.string(), type_to_obj, state->reply_handler);
 	state->type_to_obj_map = type_to_obj;
 
 
@@ -64,11 +71,12 @@ int main(int, char *argv[]) {
 	std::cin >> c;
 	std::cin.ignore();
 	if (c == 'y')
-		state->Load(base_dir/"saves/save.bin");
+		state->Load((base_dir/"saves/save.bin").string());
 	else
-		state->LoadRoom(base_dir/"rooms/room_start.txt");
+		state->LoadRoom((base_dir/"rooms/room_start.txt").string());
 
 	state->ProcessReplies();
+
 
 	while (true) {
 		std::string input;
@@ -114,7 +122,7 @@ int main(int, char *argv[]) {
 
 		state->ProcessReplies();
 
-		state->Save(base_dir/"saves/save.bin");
+		state->Save((base_dir/"saves/save.bin").string());
 	}
 
 	return 0;
